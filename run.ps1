bef88267a43c859790ba61ca2bc04e7a86490d4c
emmanuel.richard <erichard@NOMADE-01.CAP-DIGITAL.local>
2020-06-02 09:12:58 +0200

    update

--- run.ps1
+++ run.ps1
@@ -1,7 +1,7 @@
 
 function startUp() {
   Write-Host "Register Runner"
-  #c:\GitLab-Runner\.\gitlab-runner.exe register --non-interactive --executor "shell" --docker-image "mcr.microsoft.com/windows/servercore:ltsc2019" --shell "powershell" --url "https://gitlab.com/" --registration-token "nYAsaK9DqxZevC5Sn5Qn" --description "Kubernetes-Runner" --tag-list "phoenix-windows, pyphoenix-windows" --run-untagged="true" --locked="false"
+  #c:\GitLab-Runner\.\gitlab-runner.exe register --non-interactive --executor "shell" --docker-image "mcr.microsoft.com/windows/servercore:ltsc2019" --shell "powershell" --url "https://gitlab.com/" --registration-token "nYAsaK9DqxZevC5Sn5Qn" --description "Kubernetes-Runner" --tag-list "phoenix-windows-test, pyphoenix-windows-test" --run-untagged="true" --locked="false"
   c:\GitLab-Runner\.\gitlab-runner.exe register `
   --non-interactive `
   --executor "shell" `
